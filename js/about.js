7302ddcd21b64e49aed6e34203408eecbc728a1d
Omochi-S <77194852+Omochi-S@users.noreply.github.com>
2021-12-30 16:10:52 +0900

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,31 @@
+//HTML文書全体の読み込み後、ページが表示されてから実行
+window.onload = function () {
+  //厳格モード（コードをより厳しくエラーチェック）
+  "use strict";
+  //スクリーン高さ調整
+  //スクロールバーのサイズを除いた画面の高さ（document.documentElement.clientHeight)
+  let browserHeight = document.documentElement.clientHeight;
+  //bodyの高さ（document.body.clientHeight）
+  let bodyHeight = document.body.clientHeight;
+  let addHeight = browserHeight - bodyHeight;
+  let heightAbout = document.getElementById('heightAbout');
+  let heightAboutHeight = heightAbout.offsetHeight;
+  if (addHeight > 0) {
+    heightAbout.style.height = heightAboutHeight + addHeight + 'px';
+  }
+  //ヘッダーボタン/
+  document.querySelector(".headerTop").addEventListener("click", () => {
+    window.location.href = "http://" + window.location.host + "/index.html" + location.hash;
+  }, false);
+  document.querySelector(".headerResult").addEventListener("click", () => {
+    window.location.href = "http://" + window.location.host + "/result.html" + location.hash;
+  }, false);
+  document.querySelector(".headerDiagnose").addEventListener("click", () => {
+    window.location.href = "http://" + window.location.host + "/diagnose.html" + location.hash;
+  }, false);
+  document.querySelector(".linkResult").addEventListener("click", () => {
+    window.location.href = "http://" + window.location.host + "/result.html" + location.hash;
+  }, false);
+
+};
+
